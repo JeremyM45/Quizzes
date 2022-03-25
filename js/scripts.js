046c6655be227e1aeee450413c1bd5fc977d4ffd
JeremyM45 <jeremyvs1945@gmail.com>
2022-03-25 09:11:00 -0700

    add js logic to include 15 more (16 total) possible answers based off of user input to the 5 questions based around if q1===answer1

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,37 @@ $(document).ready(function() {
     let result;
     console.log(q1, q2, q3, q4, q5)
     if (q1 === "answer1" && q2 === "answer1" && q3 === "answer1" && q4 === "answer1" && q5 === "answer1") {
-      result = "Go"
+      result = "res1"
+    } else if ((q1 === "answer1" && q2 === "answer2" && q3 === "answer1" && q4 === "answer1" && q5 === "answer1")) {
+      result = "res2"
+    } else if ((q1 === "answer1" && q2 === "answer2" && q3 === "answer2" && q4 === "answer1" && q5 === "answer1")) {
+      result = "res3"
+    }  else if ((q1 === "answer1" && q2 === "answer2" && q3 === "answer2" && q4 === "answer2" && q5 === "answer1")) {
+      result = "res4"
+    } else if ((q1 === "answer1" && q2 === "answer2" && q3 === "answer2" && q4 === "answer2" && q5 === "answer2")) {
+      result = "res5"
+    } else if ((q1 === "answer1" && q2 === "answer2" && q3 === "answer1" && q4 === "answer2" && q5 === "answer1")) {
+      result = "res6"
+    }  else if ((q1 === "answer1" && q2 === "answer2" && q3 === "answer1" && q4 === "answer2" && q5 === "answer2")) {
+      result = "res7"
+    }  else if ((q1 === "answer1" && q2 === "answer2" && q3 === "answer1" && q4 === "answer1" && q5 === "answer2")) {
+      result = "res8"
+    } else if ((q1 === "answer1" && q2 === "answer1" && q3 === "answer2" && q4 === "answer1" && q5 === "answer1")) {
+      result = "res9"
+    } else if ((q1 === "answer1" && q2 === "answer1" && q3 === "answer2" && q4 === "answer2" && q5 === "answer1")) {
+      result = "res10"
+    } else if ((q1 === "answer1" && q2 === "answer1" && q3 === "answer2" && q4 === "answer2" && q5 === "answer2")) {
+      result = "res11"
+    } else if ((q1 === "answer1" && q2 === "answer1" && q3 === "answer1" && q4 === "answer2" && q5 === "answer1")) {
+      result = "res12"
+    } else if ((q1 === "answer1" && q2 === "answer1" && q3 === "answer1" && q4 === "answer2" && q5 === "answer2")) {
+      result = "res13"
+    } else if ((q1 === "answer1" && q2 === "answer1" && q3 === "answer1" && q4 === "answer1" && q5 === "answer2")) {
+      result = "res14"
+    } else if ((q1 === "answer1" && q2 === "answer2" && q3 === "answer2" && q4 === "answer1" && q5 === "answer2")) {
+      result = "res15"
+    } else if ((q1 === "answer1" && q2 === "answer1" && q3 === "answer2" && q4 === "answer1" && q5 === "answer2")) {
+      result = "res16"
     }
     $("#output").text(result);
   });
