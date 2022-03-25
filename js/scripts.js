aff244230e0165571968505d86e23f3f7dfcd257
JeremyM45 <jeremyvs1945@gmail.com>
2022-03-25 10:01:29 -0700

    refine js logic to combine 4 if else statments into 1 that asigns result to python and an else statment that asigns result to Ruby for the remaining possible combinations (now there is a total of 5 branches - 1 if, 3 if else, 1 else)

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("form#lang-quiz").submit(function(event) {
     event.preventDefault();
-    console.log("test")
+    
     const q1 = $("input:radio[name=question1]:checked").val();
     const q2 = $("input:radio[name=question2]:checked").val();
     const q3 = $("input:radio[name=question3]:checked").val();
@@ -15,23 +15,11 @@ $(document).ready(function() {
       result = "C#" 
     }  else if (q1 === "answer1" && q2 === "answer2" && q3 === "answer2" && q4 === "answer2" && q5 === "answer1" || q1 === "answer1" && q2 === "answer2" && q3 === "answer1" && q4 === "answer1" && q5 === "answer2" || q1 === "answer1" && q2 === "answer1" && q3 === "answer1" && q4 === "answer2" && q5 === "answer1" || q1 === "answer1" && q2 === "answer1" && q3 === "answer2" && q4 === "answer1" && q5 === "answer2" || q1 === "answer2" && q2 === "answer2" && q3 === "answer2" && q4 === "answer1" && q5 === "answer1" || q1 === "answer2" && q2 === "answer2" && q3 === "answer1" && q4 === "answer2" && q5 === "answer2" || q1 === "answer2" && q2 === "answer1" && q3 === "answer2" && q4 === "answer1" && q5 === "answer1" || q1 === "answer2" && q2 === "answer1" && q3 === "answer1" && q4 === "answer1" && q5 === "answer2") {
       result = "JavaScript"
-    }  else if ((q1 === "answer1" && q2 === "answer2" && q3 === "answer1" && q4 === "answer2" && q5 === "answer2")) {
-      result = "res7"
-    } else if ((q1 === "answer1" && q2 === "answer1" && q3 === "answer2" && q4 === "answer2" && q5 === "answer2")) {
-      result = "res11"
-    } else if ((q1 === "answer1" && q2 === "answer1" && q3 === "answer1" && q4 === "answer2" && q5 === "answer2")) {
-      result = "res13"
-    } else if ((q1 === "answer2" && q2 === "answer2" && q3 === "answer1" && q4 === "answer1" && q5 === "answer1")) {
-      result = "res17"
-    }  else if ((q1 === "answer2" && q2 === "answer2" && q3 === "answer2" && q4 === "answer2" && q5 === "answer1")) {
-      result = "res19"
-    }  else if ((q1 === "answer2" && q2 === "answer2" && q3 === "answer1" && q4 === "answer1" && q5 === "answer2")) {
-      result = "res23"
-    } else if ((q1 === "answer2" && q2 === "answer1" && q3 === "answer2" && q4 === "answer2" && q5 === "answer2")) {
-      result = "res26"
-    } else if ((q1 === "answer2" && q2 === "answer2" && q3 === "answer2" && q4 === "answer1" && q5 === "answer2")) {
-      result = "res30"
-    }
+    }  else if (q1 === "answer1" && q2 === "answer2" && q3 === "answer1" && q4 === "answer2" && q5 === "answer2" || q1 === "answer1" && q2 === "answer1" && q3 === "answer2" && q4 === "answer2" && q5 === "answer2" || q1 === "answer2" && q2 === "answer2" && q3 === "answer1" && q4 === "answer1" && q5 === "answer1" || q1 === "answer2" && q2 === "answer2" && q3 === "answer1" && q4 === "answer1" && q5 === "answer2") {
+      result = "Python"
+    } else  {
+      result = "Ruby"
+    } 
     $("#output").text(result);
   });
 });
